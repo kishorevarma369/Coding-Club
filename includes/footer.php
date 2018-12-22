140ebf773dd14a80e0d3f325468de780deb20618
hema allarapu <allarapuhema@gmail.com>
2018-12-22 23:03:16 +0530

    added header and footer files

---
+++ includes/footer.php
@@ -0,0 +1,34 @@
+</div>
+    <footer>
+        Kishore Varma 2018 &COPY; All Rights Reserved    
+    </footer>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script>
+        const main_nav_div=$('#main-nav')
+        const mobile_menu_close_button=$('#mobile-menu-close-button')
+        const mobile_menu_button=$('#mobile-menu-button')
+        smooth_close_func=function(){
+            main_nav_div.toggle();
+            mobile_menu_close_button.toggle();
+            mobile_menu_button.toggle();
+        }
+        $('#mobile-menu-button').click(
+            function(){
+                mobile_menu_button.toggle();
+                main_nav_div.toggle();
+                main_nav_div.css("width","50vw");
+                main_nav_div.css("padding","10%");
+                mobile_menu_close_button.toggle();
+                
+            }
+        )
+        
+        mobile_menu_close_button.click(function(){
+            main_nav_div.css("width","0");
+            main_nav_div.css("padding","0");
+            setTimeout(smooth_close_func,1000);
+        })
+    </script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Tangerine" rel="stylesheet">    
+    <title>Document</title>
+</head>
+<body>
+    <header id="main-header">
+        <p id="brand">Hema</p>
+        <i class="fa fa-bars" id="mobile-menu-button"></i>
+        <ul id="main-nav">
+            <li>Home</li>
+            <li>Hello</li>
+            <li>Contact</li>
+            <li>About</li>
+            <i class="fa fa-close" id="mobile-menu-close-button"></i>
+        </ul>
+    </header>
+    <div class="container">
