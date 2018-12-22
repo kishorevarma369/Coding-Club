3aa2f8d96d6cb07926fc0bcd45a67e2668b47655
hema allarapu <allarapuhema@gmail.com>
2018-12-22 23:15:01 +0530

    added test

---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+mail("allarapuhema@gmail.com","hello","hihi");
+?>
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque eius quidem provident necessitatibus officia, quae maxime ducimus neque, quos harum atque alias fugit dolor voluptatibus ut, odio ipsum? Laudantium, facilis?
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae odio nam alias, cupiditate voluptas dolores inventore eos exercitationem eligendi ipsam, eaque quaerat necessitatibus consequatur optio accusamus, esse provident sapiente accusantium.
